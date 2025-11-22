4fce1806aede28e5fa82295c5f4b5276bcd57832
DID53 <did53gr@gmail.com>
2025-11-23 00:37:31 +0900

    [Modify] PC SmartDesign pages

--- src/Home.js
+++ src/Home.js
@@ -10,22 +10,21 @@ export default function Home() {
       <main>
         <div className="home_row">
           <img src="images/home_title.png" className="home_photo" />
-          <a
-            className="semi-bold insta-link"
-            href="https://www.burgerking.co.kr/home"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Insta
-          </a>
+          <div className="insta-link-wrapper">
+            <a
+              className="semi-bold insta-link"
+              href="https://www.burgerking.co.kr/home"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Insta
+            </a>
+          </div>
         </div>
 
         <img src="images/hello.png" className="home_0" />
         <img src="images/home_1.png" className="home_1" />
-
-
         <Subjects />
-
         <img src="images/home_2.png" className="home_2" />
       </main>
     </div>
